6e8e889d0a34570047a7e6093d5ad9df940b4377
Sumon <m.sumon@outlook.com>
2023-07-27 13:25:16 +0600

    project initialized and gitHub login done

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,8 +1,10 @@
 import { Layout, Menu, Button } from "antd";
 const { Header } = Layout;
+import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const { data: session } = useSession();
   return (
     <Header
       style={{
@@ -38,14 +40,20 @@ const Navbar = () => {
         >
           <items>Profile</items>
         </Link>
-        <Link style={{ textDecoration: "none", color: "white" }} href="/login">
-          <items>Login</items>
-        </Link>
-        <items>
-          <Button type="primary" danger>
-            Logout
-          </Button>
-        </items>
+        {session?.user ? (
+          <items>
+            <Button onClick={() => signOut()} type="primary" danger>
+              Logout
+            </Button>
+          </items>
+        ) : (
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            href="/login"
+          >
+            <items>Login</items>
+          </Link>
+        )}
       </Menu>
     </Header>
   );
